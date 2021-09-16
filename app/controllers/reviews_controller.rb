10371e932b7be6a3d98b0fc7443f0d02efb4ed70
kie-yoshinaga <kie.103.eiwa@gmail.com>
2021-09-16 17:12:57 +0900

    投稿一覧表示、詳細表示、編集機能、削除機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,9 @@
 class ReviewsController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy ]
+  before_action :redirect_root, only: [:edit, :update, :destroy ]
+
+
+
   def index
     @reviews = Review.all
   end
@@ -16,10 +21,42 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @reviews.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    unless current_user.id == @reviews.user_id
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @reviews = Review.find(params[:id])
+  end
+
+  def update
+    @reviews.update(review_params)
+    if @reviews.save
+       redirect_to review_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def redirect_root
+    redirect_to root_path unless current_user.id == @reviews.user_id
+  end
 
   def review_params
     params.require(:review).permit(:image,:title,:detail,:genre_id,:directer,:starring).merge(user_id: current_user.id)
+  end
+
+  def set_message
+    @reviews = Review.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users
   root to: 'reviews#index'
-  resources :reviews, only:[:create, :new, :index]
+  resources :reviews, only:[:create, :new, :index, :show, :edit, :update, :destroy]
 end
